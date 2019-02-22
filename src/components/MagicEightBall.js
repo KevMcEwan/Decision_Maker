cc606ff9a3ba78052c466c8076cfcc341b676b5a
Kevin McEwan <kevinmcewan@Kevins-MacBook-Pro.local>
2019-02-22 15:50:58 +0000

    first commit: 1 container class and 2 component classes created (MagicEightBall and Dice)

---
+++ src/components/MagicEightBall.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MagicEightBall extends Component {
+   
+    render(){
+        return (
+            "MagicEightBall"
+        )
+    }
+}
+
+export default MagicEightBall;
---
+++ src/containers/Container.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Container extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            "Container"
+        )
+    }
+}
+
+export default Container;
+
