64f22c3d8307ad665026c488f4df1d0c35e0a087
Kevin McEwan <kevinmcewan@Kevins-MacBook-Pro.local>
2019-02-22 17:41:14 +0000

    logic and basic rendering complete for eight ball and dice

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,9 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 
 class Dice extends Component {
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            diceArray: [1, 2, 3, 4, 5, 6],
+            currentNumber: "Roll"
+        }
+        this.rollDice = this.rollDice.bind(this);
+    }
+
+    rollDice() {
+        this.setState({currentNumber: (this.state.diceArray[Math.floor(Math.random() * this.state.diceArray.length)])})
+    }
+
+    render() {
+        const result = this.rollDice;
         return (
-            "Dice"
+            <div className="dice-holder">
+                <div>
+                    {this.state.currentNumber}
+                </div>
+                <button className="roll-dice" onClick={this.rollDice}>Click here to roll dice!</button>
+            </div>
         )
     }
 }
--- src/components/MagicEightBall.js
+++ src/components/MagicEightBall.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 
 class MagicEightBall extends Component {
-   
+    constructor(props) {
+        super(props);
+        this.state = {
+            eightBallMessageArray: [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        ],
+            currentMessage: "Shake me!"
+        }
+        this.shakeBall = this.shakeBall.bind(this);
+    }
+
+    shakeBall() {
+        this.setState({currentMessage: (this.state.eightBallMessageArray[Math.floor(Math.random() * this.state.eightBallMessageArray.length)])})
+    }
     render(){
         return (
-            "MagicEightBall"
+            <div className="eight-ball-holder">
+                <div>
+                    {this.state.currentMessage}
+                </div>
+                <button className="shake-ball" onClick={this.shakeBall}>Click here to shake the ball!</button>
+            </div>
         )
     }
 }
 
 export default MagicEightBall;
+
